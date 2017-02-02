348e2b063e846d90612e75616f8551052f9e904e
YouriSophiyair <youri.sophiyair@efrei.net>
2017-02-02 15:09:17 +0100

    MyCampusDay first commit

---
+++ app/src/main/java/com/example/ysoph/mycampusday/ProfileScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.ysoph.mycampusday;
+
+/**
+ * Created by ysoph on 06/01/2017.
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ProfileScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.material_design_profile_screen_xml_ui_design);
+    }
+}
---
+++ app/src/test/java/com/example/ysoph/mycampusday/LoginActivityTest.java
@@ -0,0 +1,36 @@
+package com.example.ysoph.mycampusday;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ysoph on 05/01/2017.
+ */
+public class LoginActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+    @Test
+    public void onRequestPermissionsResult() throws Exception {
+
+    }
+
+    @Test
+    public void onCreateLoader() throws Exception {
+
+    }
+
+    @Test
+    public void onLoadFinished() throws Exception {
+
+    }
+
+    @Test
+    public void onLoaderReset() throws Exception {
+
+    }
+
+}
