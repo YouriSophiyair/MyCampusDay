3e2844dcd6bc1e58a5a3b3104d91423a5da2533f
YouriSophiyair <youri.sophiyair@efrei.net>
2017-02-02 16:58:51 +0100

    modifying login activity and add registration activity

---
+++ app/src/main/java/com/example/ysoph/mycampusday/RegistrationActivity.java
@@ -0,0 +1,158 @@
+package com.example.ysoph.mycampusday;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+    private static final String URL_FOR_REGISTRATION = "http://localhost/mycampusday_login_example/registration.php";
+    ProgressDialog progressDialog;
+
+    private EditText signupInputFullname, signupInputEmail, signupInputPassword, signupInputMajor, signupInputPromotion, signupInputSchool;
+    private Button btnSignUp;
+    private Button btnLinkLogin;
+    private RadioGroup genderRadioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        // Progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+
+        signupInputFullname = (EditText) findViewById(R.id.signup_input_fullname);
+        signupInputEmail = (EditText) findViewById(R.id.signup_input_email);
+        signupInputPassword = (EditText) findViewById(R.id.signup_input_password);
+        signupInputMajor = (EditText) findViewById(R.id.signup_input_major);
+        signupInputPromotion = (EditText) findViewById(R.id.signup_input_promotion);
+        signupInputSchool = (EditText) findViewById(R.id.signup_input_school);
+
+        btnSignUp = (Button) findViewById(R.id.btn_signup);
+        btnLinkLogin = (Button) findViewById(R.id.btn_link_login);
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+            }
+        });
+        btnLinkLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void submitForm() {
+
+
+        registerUser(
+                signupInputFullname.getText().toString(),
+                signupInputEmail.getText().toString(),
+                signupInputPassword.getText().toString(),
+                signupInputPromotion.getText().toString(),
+                signupInputSchool.getText().toString(),
+                signupInputMajor.getText().toString());
+    }
+
+    private void registerUser(final String fullname,  final String email, final String password,
+                              final String promotion, final String major, final String School) {
+        // Tag used to cancel the request
+        String cancel_req_tag = "register";
+
+        progressDialog.setMessage("Adding you ...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                URL_FOR_REGISTRATION, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response);
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    if (!error) {
+                        String user = jObj.getJSONObject("user").getString("fullname");
+                        Toast.makeText(getApplicationContext(), "Hi " + user +", You are successfully Added!", Toast.LENGTH_SHORT).show();
+
+                        // Launch login activity
+                        Intent intent = new Intent(
+                                RegistrationActivity.this,
+                                LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Registration Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to register url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("fullname", fullname);
+                params.put("email", email);
+                params.put("password", password);
+                params.put("promotion", promotion);
+                params.put("major", major);
+                params.put("school",school);
+                return params;
+            }
+        };
+        // Adding request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq, cancel_req_tag);
+    }
+
+    private void showDialog() {
+        if (!progressDialog.isShowing())
+            progressDialog.show();
+    }
+
+    private void hideDialog() {
+        if (progressDialog.isShowing())
+            progressDialog.dismiss();
+    }
+}
